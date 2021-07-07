560d961ead926036934c7a23470f97ce3857819b
Gillis J. de Nijs <gillis.de-nijs@capgemini.com>
2021-07-07 13:58:55 +0200

    Use "proxy" to connect to API.
    Add support for watched.
    
    Add support for delete.

--- src/Movie.js
+++ src/Movie.js
@@ -1,11 +1,52 @@
 import {Component} from "react";
 
 export class Movie extends Component {
+    constructor(props) {
+        super(props);
+        this.remove = this.remove.bind(this);
+        this.toggle = this.toggle.bind(this);
+    }
+
+    remove(movie) {
+        if (window.confirm('Are you sure you want to delete "' + movie.title + '"?'))
+            fetch(`/delete/${movie.id}`, {
+                method: 'DELETE',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                }
+            }).then(
+                (result) => {
+                    this.props.updateMovies();
+                },
+                (error) => {
+                    console.log(error);
+                });
+    }
+
+    toggle(movie) {
+        fetch(`/watched/${movie.id}`, {
+            method: 'PUT',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            }
+        }).then(
+            (result) => {
+                this.props.updateMovies();
+            },
+            (error) => {
+                console.log(error);
+            });
+    }
+
+
     render() {
         return (
             <tr>
-                <td>{this.props.title}</td>
-                <td>{this.props.watched ? "✔" : "✘"}</td>
+                <td>{this.props.movie.title}</td>
+                <td onClick={() => this.toggle(this.props.movie)}>{this.props.movie.watched ? "✔" : "✘"}</td>
+                <td onClick={() => this.remove(this.props.movie)}>🗑</td>
             </tr>
         );
     }
--- src/Movies.js
+++ src/Movies.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './Movies.css';
 import {Component} from "react";
 import Movie from "./Movie";
@@ -6,20 +5,30 @@ import Movie from "./Movie";
 class Movies extends Component {
     constructor(props) {
         super(props);
-        this.state = {movies: []};
+        this.state = {isLoaded: false, error: "", movies: []};
+        this.updateMovies = this.updateMovies.bind(this);
     }
 
     componentDidMount() {
-        fetch("http://localhost:8080/api/movie/all")
+        this.updateMovies();
+    }
+
+    updateMovies() {
+        this.setState({isLoaded: false, error: ""})
+        fetch("/all")
             .then(res => res.json())
             .then(
                 (result) => {
                     this.setState({
+                        isLoaded: true,
+                        error: "",
                         movies: result
                     });
                 },
                 (error) => {
                     this.setState({
+                        isLoaded: true,
+                        error: error,
                         movies: undefined
                     });
                 }
@@ -27,19 +36,32 @@ class Movies extends Component {
     }
 
     render() {
+        if (!this.state.isLoaded) {
+            return (
+                <div className="App loading">
+                    Loading...
+                </div>
+            )
+        } else if (this.state.error !== "") {
+            return (
+                <div className="App error">
+                    <header>Error occurred!</header>
+                    {this.state.error}
+                </div>
+            )
+        }
         return (
             <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo"/>
-                </header>
                 <table>
-                    <tbody>
+                    <thead>
                     <tr>
                         <th>Title</th>
                         <th>Watched</th>
                     </tr>
+                    </thead>
+                    <tbody>
                     {this.state.movies.map(movie => (
-                        <Movie key={movie.id} title={movie.title} watched={movie.watched}/>
+                        <Movie updateMovies={this.updateMovies} movie={movie}/>
                     ))}
                     </tbody>
                 </table>
